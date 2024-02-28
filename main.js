49da7bf84d5a44b55f7ec2d20c33d92faae5bbfc
azlanajju <muhammedazlan11@gmail.com>
2024-02-28 21:57:10 +0530

    updated responsiveness

--- main.js
+++ main.js
@@ -20,3 +20,18 @@ menuButton.addEventListener('click', navToggle);
 menuClose.addEventListener('click', closeNav);
 
 
+
+
+// scroll behaviour 
+window.addEventListener('scroll', function () {
+    const menuIcon = document.getElementById('menu-icon');
+    const scrollPosition = window.scrollY;
+  
+    if (scrollPosition > 400) {
+        menuIcon.style.color="black"
+        
+    } else {
+        menuIcon.style.color="white"
+        ;
+    }
+  });
